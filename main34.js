da90642c8c3f527be5960c7c824fc9918fdbcba2
Maham Shahid <161121868+MAHAM42@users.noreply.github.com>
2024-04-25 09:29:05 -0700

    34-Assignment
    TypeScript 45-Assignment of Q34 with Node.js

---
+++ main34.js
@@ -0,0 +1,27 @@
+"use strict";
+// ASSIGNMENT NO 34:
+/*
+Pizzas: Think of at least three kinds of your favorite pizza.
+ Store these pizza names in a array, and then use a for loop to print the name of each pizza.
+
+• Modify your for loop to print a sentence using the name of the pizza instead of printing
+ just the name of the pizza. For each pizza you should have one line of output containing a simple
+ statement like I like pepperoni pizza.
+
+• Add a line at the end of your program, outside the for loop, that states how much you like pizza.
+The output should consist of three or more lines about the kinds of pizza you like and then an additional
+ sentence, such as I really love pizza!
+ */
+console.log("ASSIGNMENT NO 34:");
+// Creating a Array:
+let favorite_pizza = ["Pepperoni ", "Malai Cheese", "Chicken Tikka"];
+// Using For-Loop:
+for (let Pizza of favorite_pizza) {
+    console.log(Pizza);
+}
+console.log("\n");
+// Print a Message outside of the for-loop:
+for (let Pizza of favorite_pizza) {
+    console.log(`I really like ${Pizza} pizza!`);
+}
+console.log(`I really love pizza!`);
